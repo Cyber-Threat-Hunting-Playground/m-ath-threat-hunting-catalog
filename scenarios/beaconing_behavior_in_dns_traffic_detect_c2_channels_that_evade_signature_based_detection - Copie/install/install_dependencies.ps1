f6b728fe237fa7ec0f0c061de0403e437f7f4322
wikijm <wikijm@users.noreply.github.com>
2026-07-05 20:21:41 +0200

    Fix scenarios folder structure to align with catalog

--- scenarios/beaconing_behavior_in_dns_traffic_detect_c2_channels_that_evade_signature_based_detection - Copie/install/install_dependencies.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = "Stop"
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-& "$scriptDir/../../../install/bootstrap_scenario_venv.ps1" -ScenarioPath "$scriptDir/.."
